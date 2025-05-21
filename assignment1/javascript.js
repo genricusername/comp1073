044445ff5df6660d9182d0d6bc9a4bbc6b5ec064
Cody McB <codymb2004@outlook.com>
2025-05-20 21:23:11 -0400

    got the button to change the text now i have to build the other buttons and figure out why when it hits 8 it isnt resetting back to 1 and changing text with that

--- assignment1/javascript.js
+++ assignment1/javascript.js
@@ -5,7 +5,7 @@ let buttonFour = 0;
 let buttonFive = 0;
 let random = false;
 
-if (random = true){
+if (random === true){
     buttonOne = Math.floor(Math.random() * 7) + 1;
     buttonTwo = Math.floor(Math.random() * 7) + 1;
     buttonThree  = Math.floor(Math.random() * 7) + 1;
@@ -16,6 +16,7 @@ if (random = true){
 
 document.querySelector("div button:nth-of-type(1)").addEventListener("click", function(){
     buttonOne++;
+    firstP(buttonOne);
 });
 
 document.querySelector("div button:nth-of-type(2)").addEventListener("click", function(){
@@ -38,20 +39,23 @@ document.querySelector("div button:nth-of-type(6)").addEventListener("click", fu
     random = true;
 });
 
-if(buttonOne === 1){
-document.querySelector("div p:nth-of-type(1)").textContent(`The turkey`);
-} else if(buttonOne === 2){
-document.querySelector("div p:nth-of-type(1)").textContent("Mom");
-} else if(buttonOne === 3){
-document.querySelector("div p:nth-of-type(1)").textContent("Dad");
-}else if(buttonOne === 4){
-document.querySelector("div p:nth-of-type(1)").textContent("the dog");
-}else if(buttonOne === 5){
-document.querySelector("div p:nth-of-type(1)").textContent("my teacher");
-}else if(buttonOne === 6){
- document.querySelector("div p:nth-of-type(1)").textContent("the elephant");  
-}else if(buttonOne === 7){
-document.querySelector("div p:nth-of-type(1)").textContent("the cat");
-}else if(buttonOne === 8){
- buttonOne = 1;
+function firstP(buttonOne){
+    if(buttonOne === 1){
+    document.querySelector("div p:nth-of-type(1)").textContent ="The turkey";
+    } else if(buttonOne === 2){
+    document.querySelector("div p:nth-of-type(1)").textContent = "Mom";
+    } else if(buttonOne === 3){
+    document.querySelector("div p:nth-of-type(1)").textContent = "Dad";
+    }else if(buttonOne === 4){
+    document.querySelector("div p:nth-of-type(1)").textContent = "the dog";
+    }else if(buttonOne === 5){
+    document.querySelector("div p:nth-of-type(1)").textContent = "my teacher" ;
+    }else if(buttonOne === 6){
+    document.querySelector("div p:nth-of-type(1)").textContent = "the elephant";  
+    }else if(buttonOne === 7){
+    document.querySelector("div p:nth-of-type(1)").textContent = "the cat";
+    }else if(buttonOne <= 8){
+    buttonOne = 1;
+    }
+    return buttonOne;
 }
